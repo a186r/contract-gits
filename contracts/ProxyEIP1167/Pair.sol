76202f7897bbafd035b69edde898d30b05484ae9
a186r <hi.aewalle@gmail.com>
2021-07-22 00:19:03 +0800

    add proxy eip1167 example

---
+++ contracts/ProxyEIP1167/Pair.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.9.0;
+
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract Pair is Ownable, Initializable {
+    address private deployer;
+    address[] private tokens;
+
+    constructor() {
+        deployer = msg.sender;
+    }
+
+    function initialize(address _tokenA, address _tokenB) public initializer {
+        require(
+            _tokenA != address(0) && _tokenB != address(0),
+            "Token must be set"
+        );
+        tokens = [_tokenA, _tokenB];
+    }
+
+    function getPair() external view returns (address[] memory) {
+        return tokens;
+    }
+}
---
+++ contracts/ProxyEIP1167/PairFactory.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "hardhat/console.sol";
+
+contract PairFactory is Ownable {
+    address public master;
+
+    event NewPair(address indexed contractAddress);
+
+    using Clones for address;
+
+    constructor(address _master) {
+        master = _master;
+    }
+
+    function getPairAddress(bytes32 salt) external view returns (address){
+        require(master != address(0), "master must be set");
+        return master.predictDeterministicAddress(salt);
+    }
+
+    function createPair(bytes32 salt) external payable {
+        master.cloneDeterministic(salt);
+    }
+}
