74a8e953ece931f2ec77a84621081d44c154663e
a186r <hi.aewalle@gmail.com>
2021-08-11 21:55:58 +0800

    update ILendingPool

--- contracts/FlashLoan/ILendingPool.sol
+++ contracts/FlashLoan/ILendingPool.sol
@@ -5,5 +5,13 @@ pragma solidity ^0.8.0;
 interface ILendingPool{
     function addressProvider() external view returns (address);
     function deposit(address _reserve, uint256 _amount, uint16 _referralCode) external payable;
+    function redeemUnderlying(address _reserve, address _user, uint256 _amount) external;
+    function borrow(address _reserve, uint256 _amount, uint256 _interestRateMode, uint16 _referralCode) external;
+    function repay(address _reserve, uint256 _amount, address _onBehalfOf) external payable;
+    function swapBorrowRateMode(address _reserve) external;
+    function rebalanceFixedBorrowRote(address _reserve, address _user) external;
+    function setUserUseReserveAsCollateral(address _reserve, bool _useAsCollateral) external;
+    function liquidationCall(address _collateral, address _reserve, address _user, uint256 _purchaseAmount, bool _receiveAToken) external payable;
+    function flashLoan(address _receiver, address _reserve, uint256 _amount, bytes calldata _params) external;
     
 }
